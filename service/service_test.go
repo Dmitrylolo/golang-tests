f3da10d2bb3873c9ee7c48bfe33849cc6c4d987b
Dmitry Chernenkov <dmitrychee@gmail.com>
2024-06-09 10:04:54 +0300

    mockery

--- service/service_test.go
+++ service/service_test.go
@@ -1,25 +1,24 @@
 package service_test
 
 import (
-	"fmt"
 	"testing"
 
 	"github.com/Dmitrylolo/golang-tests/service"
-	"github.com/stretchr/testify/mock"
+	"github.com/Dmitrylolo/golang-tests/service/mocks"
 )
 
-type smsServiceMock struct {
-	mock.Mock
-}
+// type smsServiceMock struct {
+// 	mock.Mock
+// }
 
-func (m *smsServiceMock) SendChargeNotification(value int) bool {
-	fmt.Println("Mocked charge notification")
-	args := m.Called(value)
-	return args.Bool(0)
-}
+// func (m *smsServiceMock) SendChargeNotification(value int) bool {
+// 	fmt.Println("Mocked charge notification")
+// 	args := m.Called(value)
+// 	return args.Bool(0)
+// }
 
 func TestChargeCustomer(t *testing.T) {
-	smsService := new(smsServiceMock)
+	smsService := new(mocks.MessageService)
 	smsService.On("SendChargeNotification", 100).Return(true)
 
 	myService := service.MyService{smsService}
